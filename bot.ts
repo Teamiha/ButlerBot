acb0e46c9612c4e3af248eaf76b30dd7c40a57cf
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-16 05:05:37 +0400

    add base structure

--- bot.ts
+++ bot.ts
@@ -1,1 +1,11 @@
-console.log("Hello Castle")
+import { Bot, BotError, Context, session, SessionFlavor } from "@grammyjs/bot";
+import { BOT_TOKEN } from "./token.ts";
+
+const bot = new Bot(BOT_TOKEN);
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello")
+  });
+
+
+bot.start();
