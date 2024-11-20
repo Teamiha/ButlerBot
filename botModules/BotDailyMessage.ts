144e7b71e99a46a5753753037ee7aeb44d226fbc
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-20 04:29:06 +0400

    update cron function

--- botModules/BotDailyMessage.ts
+++ botModules/BotDailyMessage.ts
@@ -3,22 +3,38 @@ import { Bot } from "https://deno.land/x/grammy/mod.ts";
 import { CHAT_ID } from "../botStatic/constance.ts";
 // import { CronSchedule } from "@DenoCrone";
 
-const dailyMessage1 = `Сейчас 10 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
+const dailyMessage1 = `Сейчас 11 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
 Cron сработал успешно.
 `
-const dailyMessage2 = `Сейчас 11 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
+const dailyMessage2 = `Сейчас 12 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
 Чистый DenoCron сработал успешно.
 `
-const dailyMessage3 = `Сейчас 12 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
+const dailyMessage3 = `Сейчас 13 часов утра, и я, такой молодец, созданный гениальный Михаилом, вам об этом сообщаю!
 DenoCron обмазанный всяким, сработал успешно. 
 `
 
+// function yerevanToUTC(yerevanHour: number): number | null {
+//     // Проверяем валидность входных данных
+//     if (!Number.isInteger(yerevanHour) || yerevanHour < 0 || yerevanHour > 23) {
+//         return null;
+//     }
+    
+//     // Ереван находится в UTC+4, поэтому вычитаем 4 часа
+//     let utcHour = yerevanHour - 4;
+    
+//     // Если получилось отрицательное число, добавляем 24 часа
+//     if (utcHour < 0) {
+//         utcHour += 24;
+//     }
+    
+//     return utcHour;
+// }
 
-
+// const targetHour = yerevanToUTC(11)
 
 export function testCronDailyMessage(bot: Bot) {
 
-    cron("0 10 * * *", async () => {
+    cron("0 7 * * *", async () => {
         try {
             console.log("Бот проснулся.");
             await bot.api.sendMessage(CHAT_ID, dailyMessage1);
@@ -30,7 +46,7 @@ export function testCronDailyMessage(bot: Bot) {
 }
 
 export async function testDenoDailyMessage(bot: Bot) {
-    Deno.cron("testMessage", "0 10 * * *", async () => { 
+    Deno.cron("testMessage", "0 8 * * *", async () => { 
         try {
             console.log("Бот проснулся.");
             await bot.api.sendMessage(CHAT_ID, dailyMessage2);
@@ -38,14 +54,13 @@ export async function testDenoDailyMessage(bot: Bot) {
         } catch (error) {
             console.error("Ошибка при отправке DenoCrone еженедельного сообщения:", error);
         }
-    }
+    }, 
 )};
 
 
 
 export async function testClaudeDailyMessage(bot: Bot) {
-
-    Deno.cron("daily-message", "0 12 * * *", async () => {
+    Deno.cron("daily-message", "0 9 * * *", async () => {
         try {
             console.log("Бот проснулся.");
             await bot.api.sendMessage(CHAT_ID, dailyMessage3);
@@ -60,3 +75,4 @@ export async function testClaudeDailyMessage(bot: Bot) {
         }
     });
 }
+
