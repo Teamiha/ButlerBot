0e4201a74db86593c95737449c89acf7e3387f4b
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-21 02:23:11 +0400

    Made reminder blanks

---
+++ botModules/botScheduleReminder.ts
@@ -0,0 +1,6 @@
+import { cron } from "@cron";
+import { Bot } from "@grammyjs/bot";
+import { CHAT_ID } from "../botStatic/constance.ts";
+import { MyContext } from "../bot.ts";
+
+const schedule: Deno.CronSchedule = { minute: 0, hour: 11, dayOfMonth: 0, month: 0, dayOfWeek: 0 };
