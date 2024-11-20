b8d7ea94274077f4d7b4b3ee57f12373c9310122
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-20 06:15:59 +0400

    add mega logging

--- botModules/botAnonymusMessage.ts
+++ botModules/botAnonymusMessage.ts
@@ -2,12 +2,16 @@ import { Bot, Context } from "@grammyjs/bot"
 
 export async function anonymusMessage(ctx: Context, bot: Bot) {
     await ctx.reply("Привет! Введи своё сообщение.");
+
+    console.log("1-Прошло первое сообщение")
     
     const test = await new Promise<string>((resolve, reject) => {
+        console.log("2-Начат процесс получение текста")
         const handler = async (ctx: Context) => {
             try {
                 if (ctx.message && ctx.message.text) {
                     resolve(ctx.message.text);
+                    console.log("3-Установка хендлера")
                 } else {
                     reject(new Error("Message is undefined"));
                 }
@@ -15,11 +19,13 @@ export async function anonymusMessage(ctx: Context, bot: Bot) {
                 reject(error); 
             }
         };
+        console.log("4-момент до Бот.Он")
         bot.on("message:text", handler);
+        console.log("5-момент после.")
     });
-
+    console.log("6-До переотправки полученого сообщения")
     await ctx.reply(`Твоё сообщение: ${test}`);
+    console.log("7-После переотправки")
 
-    return
 }
 
