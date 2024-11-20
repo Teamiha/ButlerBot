6e8fbfba6c3074ac5a47ecf2b4488043aa09fd8c
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-20 05:15:23 +0400

    update AnonSys

--- botModules/botAnonymusMessage.ts
+++ botModules/botAnonymusMessage.ts
@@ -3,9 +3,18 @@ import { Bot, Context } from "@grammyjs/bot"
 export async function anonymusMessage(ctx: Context, bot:Bot) {
     await ctx.reply("Привет! Введи своё сообщение.");
     var test = ""
-    bot.on("message:text", async (ctx) => {
-        test = ctx.message.text
-    })
+
+    // bot.on("message:text", async (ctx) => {
+    //     test = ctx.message.text
+    // })
+
+    test = await new Promise((resolve) => {
+        bot.on("message:text", async (ctx) => {
+            resolve(ctx.message.text);
+        });
+    });
+
+
     await ctx.reply(`Твоё сообщение: ${test}`);
 
 }
