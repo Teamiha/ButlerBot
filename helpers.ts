ea8371b07f443f32ee8f87febed1615b095767fe
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-23 03:34:22 +0400

    fix

--- helpers.ts
+++ helpers.ts
@@ -1,6 +1,6 @@
 import { Bot } from "@grammyjs/bot";
 import { MyContext } from "./bot.ts";
-import { getKv } from "./kvClient.ts";
+import { getKv } from "./botStatic/kvClient.ts";
 
 export async function yerevanToUTC(yerevanHour: number): Promise<number | 0> {
   if (!Number.isInteger(yerevanHour) || yerevanHour < 0 || yerevanHour > 23) {
