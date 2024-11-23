afd02a7b503404028d65ee8e4e76edc4016ec471
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-24 02:48:51 +0400

    add info

---
+++ botStatic/info.ts
@@ -0,0 +1,18 @@
+export const info = `Полезная информация.
+Наш календарь со всеми событиями: https://calendar.google.com/calendar/u/0/embed?src=castleyerevan@gmail.com&ctz=Asia/Yerevan
+Инстаграмм: https://www.instagram.com/castle_yerevan/
+Тик-ток: https://www.tiktok.com/@castleyerevan4
+
+Точки доступа wifi и пароли от них:
+Главная точка, на первом этаже:
+- name: CASTLE-WIFI
+- password: CASTLE-WIFI
+
+Точка на втором этаже:
+- name: CASTLE-WIFI
+- password: CASTLE-WIFI
+
+Точка в большом зале:
+- name: CASTLE-WIFI
+- password: CASTLE-WIFI
+`;
