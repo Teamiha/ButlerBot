9d754a3928f0afb8c86fc3eb53df20e9a0feaa58
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-24 15:21:29 +0400

    less log

--- googleCalendar/calendarDB.ts
+++ googleCalendar/calendarDB.ts
@@ -9,12 +9,12 @@ export interface OAuthTokens {
 export async function saveAdminOAuthTokens(tokens: OAuthTokens): Promise<void> {
   const kv = await Deno.openKv();
   await kv.set(["admin", "oauthTokens"], tokens);
-  console.log("Токены успешно сохранены:", tokens);
+  console.log("Токены успешно сохранены");
 }
 
 export async function getAdminOAuthTokens(): Promise<OAuthTokens | null> {
   const kv = await Deno.openKv();
   const result = await kv.get<OAuthTokens>(["admin", "oauthTokens"]);
-  console.log("Токены извлечены из KV:", result.value);
+  console.log("Токены извлечены из KV");
   return result.value || null;
 }
