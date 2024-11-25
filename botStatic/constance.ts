bb76feb4e667d16c5a20633bbafc0b4e3f53fda2
Mikhail_Svetlov <jegnum@gmail.com>
2024-11-25 21:24:39 +0400

    update chatID

--- botStatic/constance.ts
+++ botStatic/constance.ts
@@ -1,5 +1,5 @@
-export const IDESOS_GROUP_ID = -1002335442882;
-export const CASTLE_PUBLIC_GROUP_ID = 526827458;
+export const IDESOS_GROUP_ID = -1002123007165;
+export const CASTLE_PUBLIC_GROUP_ID = -1002125266002;
 export const IDESOS_CALENDAR_ID =
   "e2f38828f81c8d165481a7cdcc1ee711184fa8ada13fd8bc246f85ed715ae8a9@group.calendar.google.com";
 export const CASTLE_CALENDAR_ID =
